5ffa065d96650511f990f28435ac3f1a9448518a
Kosmas Stamos <k.stamos@prismael.com>
2023-05-18 14:58:31 +0300

    Minor Formatting in Program.cs -- scaffolding

--- ShipBunkerWindowsService/ShipBunkerWindowsService/Program.cs
+++ ShipBunkerWindowsService/ShipBunkerWindowsService/Program.cs
@@ -1,10 +1,47 @@
 using ShipBunkerWindowsService;
+using Serilog;
+using Microsoft.Extensions.Configuration.Json;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        //Logger Configuration -- 
+        Log.Logger = new LoggerConfiguration()
+           .MinimumLevel.Debug()
+           .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+           .Enrich.FromLogContext()
+           .WriteTo.File(@"C:\Users\k.stamos\Desktop\WorkRelatedLearning\ShipBunkerWindowsService\WorkerLog.txt")
+           .CreateLogger();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        IHost host = Host.CreateDefaultBuilder(args)
+            .UseWindowsService()
+            .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
+    
+    
     })
+    .UseSerilog()
     .Build();
 
-await host.RunAsync();
+        await host.RunAsync();
+    }
+}
