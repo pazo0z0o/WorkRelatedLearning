2caf0a401aa4b1d805665db6e4e2b444314457f6
Kosmas Stamos <k.stamos@prismael.com>
2023-05-18 14:51:09 +0300

    ..

--- ShipBunkerWindowsService/ShipBunkerWindowsService/Repos/IEntityRepo.cs
+++ ShipBunkerWindowsService/ShipBunkerWindowsService/Repos/IEntityRepo.cs
@@ -9,15 +9,17 @@ namespace ShipBunkerWindowsService.Repos
 {
     public interface IEntityRepo<TEntity>
     {
-        //What functions can be reused indepedently 
-         HtmlDocument DocumentLoader();
+        //What functions can be reused indepedently -- Could do it with generics but no great point in it
+        
+        //Functional Logic
+        HtmlDocument DocumentLoader();
          List<FinancialData>? ScrapingLogic(HtmlDocument loadedDoc);
          
          void CsvOutput(List<FinancialData> ScrapingData);
-        
-        
+
         //------------------------------------------------------------//
-         bool DayOfWeekCheck();
+        //Additional small ones 
+        bool DayOfWeekCheck();
          string? IsoFormatConverter(string DayOfMonth); 
 
 
