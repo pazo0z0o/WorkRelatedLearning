c108caf97309e84a0978bf4a432496b955378d42
Kosmas Stamos <k.stamos@prismael.com>
2023-05-30 12:47:53 +0300

    finished global Usings, Api HttpVerbs,endpoint mappings

---
+++ DapperDemo/DapperDemo/DapperDemo/DapperDemo/API.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace DapperDemo;
+
+public static class API
+{
+    //This is instead of a controller class
+    public static void ConfigureApi(this WebApplication app)
+    {
+        //All of my APIs endpoints mapping -- remains public since the configuration needs to be used for the routing
+        app.MapGet(pattern: "/Users", GetUsers);
+        app.MapGet(pattern: "/Users{id}", GetUserById);
+        app.MapPost(pattern: "/Users", InsertUser);
+        app.MapPut(pattern: "/Users", UpdateUser);
+        app.MapDelete(pattern: "/Users", DeleteUser);
+    }
+
+    private static async Task<IResult> GetUsers(IUserData data)
+    {
+        try
+        {
+            return Results.Ok(await data.GetAllUsers()); //proper return codes
+        }
+        catch (Exception ex)
+        {
+
+            return Results.Problem(ex.Message);
+        }
+
+    }
+    //Good Practices 
+    private static async Task<IResult> GetUserById(int id, IUserData data)
+    {
+        try
+        {
+            var results = await data.GetById(id);
+            //measure if we don't get a result by a user's Id 
+            if (results == null) { return Results.NotFound(); }
+
+            return Results.Ok(results);
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+    private static async Task<IResult> InsertUser(UserModel user, IUserData data)
+    {
+        try
+        {
+            await data.InsertUser(user);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }    
+    }
+    
+    private static async Task<IResult> UpdateUser(UserModel user, IUserData data)
+    {
+        try
+        {
+            await data.UpdateUser(user);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> DeleteUser(int id, IUserData data)
+    {
+        try
+        {
+            await data.DeleteUser(id);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+}
---
+++ DapperDemo/DapperDemo/DapperDemo/DapperDemo/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using DataAccess.Data;
+global using DataAccess.Models;
+
+
+//class to just put all the global usings of our program 
+//so that they are used universally by all our other classes/elements
