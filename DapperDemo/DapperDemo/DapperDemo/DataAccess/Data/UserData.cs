9392a72d4eceacae767f3eabb6cab4f64ed6a3db
Kosmas Stamos <k.stamos@prismael.com>
2023-05-30 10:29:10 +0300

    XML notations

--- DapperDemo/DapperDemo/DapperDemo/DataAccess/Data/UserData.cs
+++ DapperDemo/DapperDemo/DapperDemo/DataAccess/Data/UserData.cs
@@ -17,28 +17,47 @@ public class UserData : IUserData
         _db = db;
     }
 
+    ///<summary>Handles the GetAll sql query</summary>
+    /// <returns>A <see cref="Task{IEnumerable{UserModel}}"/>  That is our return list for the GetAll crud function</returns>
     public Task<IEnumerable<UserModel>> GetAllUsers() =>
         _db.LoadData<UserModel, dynamic>
         (storedProcedure: "dbo.spUser", new {  });
 
+    ///<summary>Handles the GetById sql query</summary>
+    /// <param name="id">The id that corresponds to the table element defined in the stored procedure </param>
+    /// <returns>A <see cref="IEnumerable{UserModel}" /> That is our return UserModel for the GetAId crud function</returns>
     public async Task<UserModel?> GetById(int id)
-    {   //
+    {   
         var results = await _db.LoadData<UserModel, dynamic>
             (storedProcedure: "dbo.spUser_Get", new { Id = id });
 
         return results.FirstOrDefault();
     }
     //Insert-Create : It mapped the usermodel already here,
-    //hence why user.FirstName is an option
+    //hence why user.FirstName, user.LastName are an option
+
+    ///<summary>Handles the GetAll sql query</summary>
+    /// <param name="user">The user pamater fed to the stored procedure of our choice </param>
+    ///<typeparam name="UserModel"> UserModel type of user param</typeparam>
+    /// <returns>A <see cref="Task{IEnumerable}type<typeparam name="UserModel"}" /> That is our return list for the GetAll crud function</returns>
     public Task InsertUser(UserModel user) =>
         _db.SaveData(storedProcedure: "spUser_Insert"
-            , new { user.FirstName, user.LastName });
+            ,new { user.FirstName, user.LastName });
 
     //Update 
+
+    ///<summary>Handles the Update sql query</summary>
+    /// <param name="user">Accepts a UserModel </param>
+    ///<typeparam name="UserModel"> UserModel type of user param</typeparam>
+    /// <returns>A <see cref="Task{IEnumerable}" /> That is our return list for the GetAll crud function</returns>
     public Task UpdateUser(UserModel user) =>
         _db.SaveData(storedProcedure: "spUser_Update", user);
 
     //Delete
+
+    ///<summary>Handles the Delete sql query</summary>
+    /// <param name="id">The stored procedure of our choice </param>
+    /// <returns>A <see cref="Task" /> Returns an awaitable void task</returns>
     public Task DeleteUser(int id) =>
         _db.SaveData(storedProcedure: "spUser_Delete", new { Id = id });
 
--- DapperDemo/DapperDemo/DapperDemo/DataAccess/DbAccess/SqlDataAccess.cs
+++ DapperDemo/DapperDemo/DapperDemo/DataAccess/DbAccess/SqlDataAccess.cs
@@ -16,20 +16,28 @@ public class SqlDataAccess : ISqlDataAccess
     //Just 2 classes with a connection and a query execution(async) are enough for
     //data access. They will be invoked multiple times depending on the occassion
 
-    //<summary>Handles the GetAll or Get sql queries</summary>
+    ///<summary>Handles the GetAll or Get sql queries</summary>
+    /// <param name="storedProcedure">The stored procedure of our choice </param>
+    /// <param name="parameters"type<typeparam name="T">Parameters of input in our functions</param>
+    /// <param name="connectionId"> The connection string for the database</param>
+    /// <returns>A <see cref="Task{IEnumerable<T>}" /> That is our return list for the GetById or GetAll crud functions</returns>
     public async Task<IEnumerable<T>> LoadData<T, U>(
         string storedProcedure,
         U parameters,
         string connectionId = "Default")
-    {//used to create the connection
+    {//used to create the connection -- Uses IDbConnection instead of config. and  
         using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
 
         //used to execute the storedProcedure we give to it
         return await connection.QueryAsync<T>(storedProcedure
-                         , parameters, commandType: CommandType.StoredProcedure);
+                    ,parameters, commandType: CommandType.StoredProcedure);
     }
 
-    //<summary>Handles the Insert,Delete,Update sql queries</summary>
+    ///<summary>Handles the Insert,Update,Delete sql queries</summary>
+    /// <param name="storedProcedure">The stored procedure of our choice </param>
+    /// <param name="parameters" type<typeparam name="T">Parameters of input in our functions</param>
+    /// <param name="connectionId"> The connection string for the database</param>
+    /// <returns>A <see cref="Task"/> That is our return type for the Insert,Update,Delete crud operations</returns>
     public async Task SaveData<T>(string storedProcedure,
                                   T parameters,
                                   string connectionId = "Default")
