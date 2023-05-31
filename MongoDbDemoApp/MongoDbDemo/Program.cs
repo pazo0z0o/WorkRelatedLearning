6083b22d22319313f70eddce4ace4c35ae442ee7
Kosmas Stamos <k.stamos@prismael.com>
2023-05-31 17:20:35 +0300

    mongodb fixed

--- MongoDbDemoApp/MongoDbDemo/Program.cs
+++ MongoDbDemoApp/MongoDbDemo/Program.cs
@@ -13,9 +13,25 @@ namespace MongoDbDemo
         static void Main(string[] args)
         {
             MongoCRUD db = new MongoCRUD("AddressBook");
+            //What if we add a new property after declaring 2-3 documents
+            PersonModel person = new PersonModel
+            {   Name = "Rider",
+                LastName = "Biden",
+                
+                PrimaryAddress = new AddressModel
+                {
+                StreetAddress = "101 Oak Street",
+                City = "Boston",
+                State = "Massachuchets",
+                ZiplCode = "12345"
+                }
+            };
+            
+            db.InsertRecord("Users", person);
+            //The new person document is going to ALSO have the AddressModel object but not the other 2 previous
             
             //Do an if, and if ID = null , THEN insert) = good practice 
-            db.InsertRecord("Users",new PersonModel { Name="Kosmas" , LastName = "Stamos"});
+            //db.InsertRecord("Users",new PersonModel { Name="Popi" , LastName = "Popper"});
             
             Console.ReadLine();
         }
@@ -24,10 +40,21 @@ namespace MongoDbDemo
     public class PersonModel
     {
         [BsonId] // unique identifier of the model class
+        public Guid Id { get; set; }
+        
         public string Name { get; set; }
         public string LastName { get; set; }
+        public AddressModel PrimaryAddress { get; set; }
+    }
 
-        public Guid Id { get; set; }
+    public class AddressModel 
+    { 
+    public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZiplCode { get; set; }
+    
+    
     }
 
 
