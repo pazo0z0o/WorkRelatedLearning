c83c5c3ca06362cc91e93a2f5d4c04684c667b3e
Kosmas Stamos <k.stamos@prismael.com>
2023-06-01 18:17:06 +0300

    To be contiinued

---
+++ MongoExample/MongoExample/Playlist.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoExample
+{
+    public class Playlist
+    {
+        public ObjectId _id { get; set; }
+
+        [BsonElement("user")]
+        public string? Username { get; set; } = null!;
+        public List<string> Items { get; set; } = null!;
+
+        public Playlist(string username, List<string> movieIds)
+        {
+            this.Username = username;
+            this.Items = movieIds;
+        }
+
+    }
+}
---
+++ MongoExample/MongoExample/Program.cs
@@ -0,0 +1,109 @@
+﻿using MongoDB.Driver;
+using MongoExample;
+
+MongoClient client = new MongoClient("mongodb+srv://kossmasstamoss:070891p%40p%40r0P0ulos@trainingcluster.ijf9u0g.mongodb.net/");
+
+//List<string> databases = client.ListDatabaseNames().ToList();           //just checking if my connection is working
+//foreach (var database in databases) { Console.WriteLine(database); }        
+
+var playlistCollection = client.GetDatabase("sample_mflix").GetCollection<Playlist>("playlist");
+
+List<string> movieList = new List<string>();
+movieList.Add("Gladiator");
+
+//playlistCollection.InsertOne(new Playlist("nicraboy",movieList));
+
+//Filtering for later use -- Could be a different file with many of them, if we make them abstract enough
+FilterDefinition<Playlist> filter = Builders<Playlist>.Filter.Eq("Username", "nicraboy");
+
+List<Playlist> results = playlistCollection.Find(filter).ToList();
+foreach (Playlist entry in results)
+{
+    Console.WriteLine(entry.Items);
+}
+//update operation with filter for already existing value
+UpdateDefinition<Playlist> update = Builders<Playlist>.Update.AddToSet<string>("Items", "5678");
+
+playlistCollection.UpdateOne(filter, update);
+results = playlistCollection.Find(filter).ToList();
+foreach (var entry in results)
+{
+    Console.WriteLine(entry.Items);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
