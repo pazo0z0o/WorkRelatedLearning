921a0dc332bc05bf4d7736763ec1399d94421775
Kosmas_Stamos <kossmasstamoss@gmail.com>
2023-07-22 13:04:16 +0300

    caught up again

---
+++ UdemyCourse_IS4/WebApp_UnderTheHood/WebApp/Data/WorkDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Data
+{
+    public class WorkDbContext : IdentityDbContext
+    {
+        public WorkDbContext(DbContextOptions<WorkDbContext> options) : base(options)
+        {
+                
+        }
+    }
+}
--- UdemyCourse_IS4/WebApp_UnderTheHood/WebApp/Program.cs
+++ UdemyCourse_IS4/WebApp_UnderTheHood/WebApp/Program.cs
@@ -1,7 +1,47 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
 
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+#region AddDbContexts 
+builder.Services.AddDbContext<ApplicationDbContext>(options => {
+    options.UseSqlServer(configuration.GetConnectionString("Home"));
+
+
+});
+//WORK dbContext
+//builder.Services.AddDbContext<WorkDbContext>(options => {
+//    options.UseSqlServer(configuration.GetConnectionString("Work"));
+
+//});
+
+#endregion
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 8;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+
+    options.User.RequireUniqueEmail = true;
+    options.SignIn.RequireConfirmedEmail = true;
+})
+       .AddEntityFrameworkStores<ApplicationDbContext>()
+       .AddDefaultTokenProviders(); //provider for email confirmation token
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Accounts/Login";
+    options.AccessDeniedPath = "/Accounts/AccessDenied";
+});
+
 
 var app = builder.Build();
 
@@ -17,7 +57,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
